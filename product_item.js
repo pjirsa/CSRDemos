8aa12da062d4441c80aa49d66d5b68e175d13687
Phil Jirsa <pjirsa@gmail.com>
2013-10-29 23:12:25 -0500

    first commit

---
+++ product_item.js
@@ -0,0 +1,16 @@
+﻿(function(){
+	var itemOverrideCtx = {};
+	itemOverrideCtx.Templates = {};
+	
+	itemOverrideCtx.Templates.Item = ProductItemOverride;
+	
+	SPClientTemplates.TemplateManager.RegisterTemplateOverrides(itemOverrideCtx);			
+	
+})();
+
+
+function ProductItemOverride(itemCtx){
+	var templateSrc = $("#product-template").html();
+	var itemTemplate = Handlebars.compile(templateSrc);
+	return itemTemplate(itemCtx.CurrentItem);
+}
---
+++ store_view.js
@@ -0,0 +1,15 @@
+﻿(function(){
+	var viewCtx = {};
+	viewCtx.Templates = {};
+	
+	viewCtx.Templates.Body = StoreBody;
+		
+	SPClientTemplates.TemplateManager.RegisterTemplateOverrides(viewCtx);
+})();
+
+function StoreBody(viewCtx){
+	var source = $("#store-template").html();
+	var storeTemplate = Handlebars.compile(source);
+	
+	return storeTemplate(viewCtx.ListData);
+}
