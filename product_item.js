c793196dd7c79034e02509fc8d00e9e538083be1
Phil Jirsa <pjirsa@gmail.com>
2013-10-31 21:16:31 -0500

    updates

--- product_item.js
+++ product_item.js
@@ -1,16 +1,51 @@
-﻿(function(){
+﻿var itemTemplate;
+
+(function(){
 	var itemOverrideCtx = {};
 	itemOverrideCtx.Templates = {};
 	
 	itemOverrideCtx.Templates.Item = ProductItemOverride;
 	
+	itemOverrideCtx.Templates.Fields = {
+		"BusinessEntityID" : {
+			"NewForm" : ProductFieldOverride,
+			"EditForm" : ProductFieldOverride,
+			"View" : ProductFieldOverride
+		}
+	};
+	
+	
+	itemOverrideCtx.Templates.OnPreRender = ProductItemPreRender;
+	
+//	itemOverrideCtx.Templates.Group = GroupProductOverride;
+	
 	SPClientTemplates.TemplateManager.RegisterTemplateOverrides(itemOverrideCtx);			
 	
+
 })();
 
 
-function ProductItemOverride(itemCtx){
+function ProductFieldOverride(){
+	return "";
+}
+
+function ProductItemPreRender(){
 	var templateSrc = $("#product-template").html();
-	var itemTemplate = Handlebars.compile(templateSrc);
+	itemTemplate = Handlebars.compile(templateSrc);	
+}
+
+function ProductItemOverride(itemCtx){	
 	return itemTemplate(itemCtx.CurrentItem);
+}
+
+function JsonItemOverride(itemCtx){
+	var result = "";
+	result += "<div>";
+	result += JSON.stringify(itemCtx.CurrentItem);
+	result += "</div>";
+	return result;
+}
+
+function GroupProductOverride(){
+	return "Phils Group";
 }
--- store_view.js
+++ store_view.js
@@ -1,15 +1,39 @@
-﻿(function(){
+﻿var storeBodyTemplate;
+
+(function(){
 	var viewCtx = {};
 	viewCtx.Templates = {};
 	
 	viewCtx.Templates.Body = StoreBody;
-		
+	viewCtx.Templates.OnPreRender = CompileStoreTemplate;
+	viewCtx.Templates.OnPostRender = RenderMap;
+			
 	SPClientTemplates.TemplateManager.RegisterTemplateOverrides(viewCtx);
 })();
 
-function StoreBody(viewCtx){
+function CompileStoreTemplate(){
 	var source = $("#store-template").html();
-	var storeTemplate = Handlebars.compile(source);
-	
-	return storeTemplate(viewCtx.ListData);
+	storeBodyTemplate = Handlebars.compile(source);
 }
+
+function RenderMap(inCtx){
+	var map = new mxn.Mapstraction('map-canvas', 'googlev3');
+	var bounds = new mxn.BoundingBox(40.0,-89.0,42.0,-87.0);
+	for (var i = 0; i < inCtx.ListData.Row.length; ++i){
+		var item = inCtx.ListData.Row[i];
+		var pt = new mxn.LatLonPoint(item.Latitude, item.Longitude);
+		var marker = new mxn.Marker(pt);
+		marker.setLabel(item.Title);
+		
+		map.addMarker(marker);
+	}
+
+	map.setBounds(bounds);	
+
+	
+}
+
+function StoreBody(viewCtx){
+	return storeBodyTemplate(viewCtx.ListData);
+}
+
